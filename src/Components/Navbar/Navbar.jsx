c5e2c29c1cb258eb5fbb2fdd465502f732962d88
AdnanAraf <arafadnan10@gmail.com>
2023-10-06 10:46:25 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="bg-blue-300 flex">
+        <h1 className="px-[60px] p-[20px] font-semibold">
+          Enjoy 10% off everything with code GIFT2023
+        </h1>
+        <button className="bg-white h-[30px] w-[120px] rounded-lg mt-[15px] ">
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
